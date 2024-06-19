40e3cc66f3deda8fc06124ec78f9d0d8c5810ea4
kjirou <kjirou.web@gmail.com>
2024-06-19 11:03:59 +0900

    wip

--- src/index.ts
+++ src/index.ts
@@ -10,20 +10,105 @@
 
 // TODO: Pドリンク
 // TODO: サポートアビリティ
-// TODO: レッスン中
+// TODO: レッスン内の小目標
+// TODO: データの永続化サポート
 // TODO: コンテスト、後のためのメモ
 //       - AIの挙動を解読する必要がある、多少眺めた限りだとわからなかった
 //       - レッスン中に放置するとカードがうっすら光っておすすめカードを教えてくれるが、それがコンテストと同じAIかもしれない
 //         - もしそうだとすると、AIはサーバ側ではなくてクライアント側が計算しているのかもしれない
 
-import { Lesson } from "./types";
+import { Idol, IdolInProduction, Lesson, LessonGamePlay } from "./types";
 
-const initializeLesson = (params: {
+const createIdol = (params: { idolInProduction: IdolInProduction }): Idol => {
+  return {
+    life: params.idolInProduction.life,
+    original: params.idolInProduction,
+    totalCardUsageCount: 0,
+    vitality: 0,
+  };
+};
+
+const createLesson = (params: {
+  idolInProduction: IdolInProduction;
   lastTurnNumber: Lesson["lastTurnNumber"];
 }): Lesson => {
   return {
+    idol: createIdol({
+      idolInProduction: params.idolInProduction,
+    }),
     lastTurnNumber: params.lastTurnNumber,
     score: 0,
     turnNumber: 1,
   };
 };
+
+export const createLessonGamePlay = (): LessonGamePlay => {};
+
+/**
+ * レッスンのターンを開始する
+ *
+ * - レッスン開始時に関わる処理も含む
+ */
+export const startLessonTurn = (
+  lessonGamePlay: LessonGamePlay,
+): LessonGamePlay => {
+  let newLessonGamePlay = lessonGamePlay;
+  // TODO: レッスン開始時トリガー
+  // TODO: ターン開始時トリガー
+  return newLessonGamePlay;
+};
+
+// const playCard
+// const previewCardUsage
+
+/**
+ * スキルカードを選択する
+ *
+ * - スキルカードを選択し、結果のプレビュー表示または使用を行う
+ * - TODO: スキルカード使用による状態修正増加時トリガーによる効果も、プレビューに反映されてる？
+ */
+export const selectCard = (
+  lessonGamePlay: LessonGamePlay,
+  cardInHandIndex: number,
+): LessonGamePlay => {
+  let newLessonGamePlay = lessonGamePlay;
+  // TODO: もしプレビュー表示中で同じカード選択なら or not
+  // TODO: スキルカード使用
+  // TODO: スキルカード使用時トリガー
+  // TODO: スキルカード使用による状態修正増加時トリガー
+  return newLessonGamePlay;
+};
+
+/**
+ * レッスンのターンを終了する
+ *
+ * - レッスン終了時に関わる処理は、現在はなさそう
+ */
+export const endLessonTurn = (
+  lessonGamePlay: LessonGamePlay,
+): LessonGamePlay => {
+  let newLessonGamePlay = lessonGamePlay;
+  // TODO: ターン終了時トリガー
+  // TODO: 小目標発動
+  return newLessonGamePlay;
+};
+
+//
+// UI側での想定の呼び出し方
+//
+// ```
+// const lessonGamePlay = createLessonGamePlay();
+//
+// const onStartGame = () => {
+//   setState(startLessonTurn(lessonGamePlay))
+// };
+//
+// const onPressCard = () => {
+//   setState(selectCard(lessonGamePlay, cardInHandIndex))
+// };
+//
+// const onPress休憩 = () => {
+//   setState(休憩する(lessonGamePlay));
+// };
+// ```
+//
